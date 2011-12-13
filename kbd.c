dc6f27c6ab8ee8fd49bac87bba50c00a9f4aee23
Ralf Dragon <dragon@tnt.uni-hannover.de>
2011-12-13 21:55:20 +0100

    remove assert

--- kbd.c
+++ kbd.c
@@ -232,7 +232,10 @@ unsigned char keycode;
 		key_down	= ( key <= 63 );
 		if (!key_down)
 			key	= (key-63)&0x3F; // convert key code for key up
-                assert(key <= 63);
+                if (key <= 63) {
+                     fprintf(stdout, "Invalid key: %02d, setting to R\n", key);
+                     key = KEY_R;
+                }
 	        keycode         = freedom_kbd[key];
 		if (debug)
 			fprintf(stdout, "%02d %02d\n", buf[0], keycode);
